eb7d483d38859b61ed9a2e1d75b9f6397753b8f6
seogenie <seogenie@naver.com>
2018-04-18 17:05:29 +0900

    #13 class (smart) vs functional (dumb)

--- src/Movie.js
+++ src/Movie.js
@@ -1,34 +1,29 @@
-import React, { Component } from 'react';
+import React from 'react';
 import propTypes from 'prop-types';
 import './Movie.css';
 
-class Movie extends Component {
-
-  static propTypes = {
-    title: propTypes.string.isRequired,
-    poster: propTypes.string
-  }
-
-  render() {
-    console.log(this.props)    
-    return (
-      <div>
-        <MoviePoster poster={this.props.poster} />
-        <h1>{this.props.title}</h1>
-      </div>
-    );
-  }
+function Movie({title, poster}){
+  return (
+    <div>
+      <MoviePoster poster={poster} />
+      <h1>{title}</h1>
+    </div>
+  );
 }
 
-class MoviePoster extends Component{
-  static propTypes = {
-    poster: propTypes.string.isRequired
-  }
-  render() {
-    return(
-      <img src={this.props.poster} alt=" "/>
-    )
-  }
+function MoviePoster({poster}){
+  return(
+    <img src={poster} alt=" "/>
+  )
+}
+
+Movie.propTypes = {
+  title: propTypes.string.isRequired,
+  poster: propTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+  poster: propTypes.string.isRequired
 }
 
 export default Movie;
