f81e4b02e714f180f11ae6a51a51b80eec906486
seogenie <seogenie@naver.com>
2018-04-18 17:48:10 +0900

    #14 what is Promise

--- src/App.js
+++ src/App.js
@@ -3,47 +3,13 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
+  state = {}
   
-  state = { }
-
-  componentWillMount(){
-    console.log('(1)will mount')
-  }
-
   componentDidMount(){
-    console.log('(3)did mount')
-
-    setTimeout(()=> {
-      this.setState({
-        movies: [
-          {
-            id: 1,
-            title: "title1",
-            poster: "a.jpg"
-          },
-          {
-            id: 2,
-            title: "title2",
-            poster: "a2.jpg"
-          },
-          {
-            id: 3,
-            title: "title3",
-            poster: "a3.jpg"
-          },
-          {
-            id: 4,
-            title: "title4",
-            poster: "a4.jpg"
-          },
-                    {
-            title: "add",
-            poster:"gh.jpg"
-          }
-        ]
-      })
-    }, 2000)
-
+    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(responseOFfetch => responseOFfetch.json())
+    .then(json => console.log(json))
+    .catch(err => console.log(err))
   }
 
   _renderMovies = () => {
