2471cac7569d452effe52c3ea27eb94ecae47147
seogenie <seogenie@naver.com>
2018-04-18 16:45:07 +0900

    #12 loading state// _userFn

--- src/App.js
+++ src/App.js
@@ -4,31 +4,7 @@ import Movie from './Movie'
 
 class App extends Component {
   
-  state = {
-    greeting: 'hello~!',
-    movies: [
-      {
-        id: 1,
-        title: "title1",
-        poster: "a.jpg"
-      },
-      {
-        id: 2,
-        title: "title2",
-        poster: "a2.jpg"
-      },
-      {
-        id: 3,
-        title: "title3",
-        poster: "a3.jpg"
-      },
-      {
-        id: 4,
-        title: "title4",
-        poster: "a4.jpg"
-      },
-    ]
-  }
+  state = { }
 
   componentWillMount(){
     console.log('(1)will mount')
@@ -40,19 +16,42 @@ class App extends Component {
     setTimeout(()=> {
       this.setState({
         movies: [
-          
           {
+            id: 1,
+            title: "title1",
+            poster: "a.jpg"
+          },
+          {
+            id: 2,
+            title: "title2",
+            poster: "a2.jpg"
+          },
+          {
+            id: 3,
+            title: "title3",
+            poster: "a3.jpg"
+          },
+          {
+            id: 4,
+            title: "title4",
+            poster: "a4.jpg"
+          },
+                    {
             title: "add",
             poster:"gh.jpg"
-          },
-
-          ...this.state.movies
+          }
         ]
       })
     }, 2000)
 
   }
 
+  _renderMovies = () => {
+    const movies = this.state.movies.map((movie, index) => {
+      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    })
+    return movies
+  }
 
   render() {
 
@@ -61,9 +60,7 @@ class App extends Component {
     console.log('(2)render by App')
     return (
       <div className="App">
-        {this.state.movies.map((movie, index) => {
-          return <Movie title={movie.title} poster={movie.poster} key={index} />
-        })}
+        {this.state.movies ? this._renderMovies() : "로딩중.." }
       </div>
     );
   }
