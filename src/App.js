6f265a23befae8f7c0507b4ad2708bcfabf838f6
seogenie <seogenie@naver.com>
2018-04-19 09:47:09 +0900

    #16 Async Await in React

--- src/App.js
+++ src/App.js
@@ -3,30 +3,40 @@ import './App.css';
 import Movie from './Movie'
 
 class App extends Component {
+
   state = {}
   
   componentDidMount(){
-    fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
-    .then(responseOFfetch => responseOFfetch.json())
-    .then(json => console.log(json))
-    .catch(err => console.log(err))
+    this._getMovies();
   }
 
   _renderMovies = () => {
-    const movies = this.state.movies.map((movie, index) => {
-      return <Movie title={movie.title} poster={movie.poster} key={index} />
+    const movies = this.state.movies.map((movie) => {
+      console.log(movie)
+      return <Movie title={movie.title} poster={movie.large_cover_image} key={movie.id}/>
     })
     return movies
   }
 
+  _getMovies = async () => {
+    const movies = await this._callApi()
+    this.setState({
+      movies
+    })
+  }
+
+  _callApi = () => {
+    return fetch('https://yts.am/api/v2/list_movies.json?sort_by=rating')
+    .then(responseOFfetch => responseOFfetch.json())
+    .then(json => json.data.movies)
+    .catch(err => console.log(err))
+  }
+
   render() {
-
-    
-
     console.log('(2)render by App')
     return (
       <div className="App">
-        {this.state.movies ? this._renderMovies() : "로딩중.." }
+        {this.state.movies ? this._renderMovies() : "로딩중....." }
       </div>
     );
   }
